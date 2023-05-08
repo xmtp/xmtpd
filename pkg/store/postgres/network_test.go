da5684b4dc74a6a9b8a3938e6d5d3fec8eebbc33
Steven Normore <snormore@gmail.com>
2023-05-08 07:50:41 -0400

    postgres: remove inline time.Sleep in network test

--- pkg/store/postgres/network_test.go
+++ pkg/store/postgres/network_test.go
@@ -2,9 +2,7 @@ package postgresstore_test
 
 import (
 	"fmt"
-	"os"
 	"testing"
-	"time"
 
 	ntest "github.com/xmtp/xmtpd/pkg/node/testing"
 )
@@ -22,11 +20,6 @@ func Test_RandomNodeAndTopicSpraying(t *testing.T) {
 		{3, 10, 300},
 		{5, 3, 100},
 		{10, 5, 100},
-	}
-	if os.Getenv("CI") == "true" {
-		// It seems we need to give postgres a sec to start up in CI
-		// getting postgres dial errors too often.
-		time.Sleep(5 * time.Second)
 	}
 	for i, tc := range tcs {
 		tc := tc

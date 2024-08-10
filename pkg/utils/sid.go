aecf1c39741e08ef94d9ddb1015f90518a0d5e7b
Richard Hua <rich@rkhua.com>
2024-08-09 17:31:14 -0700

    Flesh out SID utils

--- pkg/utils/sid.go
+++ pkg/utils/sid.go
@@ -1,18 +1,36 @@
 package utils
 
+// Operations to marshall to/from SIDs.
+// It is recommended not to use these directly - instead use a wrapper type that
+// performs the relevant error handling.
+
 const (
-	nodeIDMask  uint64 = 0xFFFF << 48
+	nodeIDBits  uint64 = 16
+	localIDBits uint64 = 64 - nodeIDBits
+	nodeIDMask  uint64 = 0xFFFF << localIDBits
 	localIDMask uint64 = ^nodeIDMask
 )
 
-// Converts a local serial ID from the database into a global SID with a node ID prefix
-func SID(localID int64) uint64 {
-	nodeMask := uint64(localID) & nodeIDMask
-	if localID < 0 || nodeMask != 0 {
-		// Either indicates ID exhaustion or developer error -
-		// the service should not continue running either way
-		panic("Invalid local ID")
-	}
-	// TODO(rich): Plumb through and set node ID
-	return uint64(localID)
+func IsValidNodeID(nodeID int32) bool {
+	return nodeID >= 0 && nodeID>>nodeIDBits == 0
+}
+
+func IsValidLocalID(localID int64) bool {
+	return localID > 0 && localID>>localIDBits == 0
+}
+
+func IsValidSID(sid uint64) bool {
+	return IsValidNodeID(NodeID(sid)) && IsValidLocalID(LocalID(sid))
+}
+
+func NodeID(sid uint64) int32 {
+	return int32(sid >> localIDBits)
+}
+
+func LocalID(sid uint64) int64 {
+	return int64(sid & localIDMask)
+}
+
+func SID(nodeID int32, localID int64) uint64 {
+	return uint64(nodeID)<<localIDBits | uint64(localID)
 }

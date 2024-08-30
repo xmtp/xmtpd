7b26a1f06478e855749a75261d87d90090642ae2
Martin Kysel <code@martinkysel.com>
2024-08-29 19:34:54 -0600

    Expose all config options as env variables

--- pkg/config/options.go
+++ pkg/config/options.go
@@ -5,36 +5,36 @@ import (
 )
 
 type ApiOptions struct {
-	Port int `short:"p" long:"port" description:"Port to listen on" default:"5050"`
+	Port int `short:"p" long:"port" description:"Port to listen on" env:"XMTPD_API_PORT" default:"5050"`
 }
 
 type ContractsOptions struct {
-	RpcUrl                  string        `long:"rpc-url"          description:"Blockchain RPC URL"`
-	NodesContractAddress    string        `long:"nodes-address"    description:"Node contract address"`
-	MessagesContractAddress string        `long:"messages-address" description:"Message contract address"`
-	ChainID                 int           `long:"chain-id"         description:"Chain ID for the appchain"               default:"31337"`
-	RefreshInterval         time.Duration `long:"refresh-interval" description:"Refresh interval for the nodes registry" default:"60s"`
+	RpcUrl                  string        `long:"rpc-url"          env:"XMTPD_CONTRACTS_RPC_URL"          description:"Blockchain RPC URL"`
+	NodesContractAddress    string        `long:"nodes-address"    env:"XMTPD_CONTRACTS_NODES_ADDRESS"    description:"Node contract address"`
+	MessagesContractAddress string        `long:"messages-address" env:"XMTPD_CONTRACTS_MESSAGES_ADDRESS" description:"Message contract address"`
+	ChainID                 int           `long:"chain-id"         env:"XMTPD_CONTRACTS_CHAIN_ID"         description:"Chain ID for the appchain"               default:"31337"`
+	RefreshInterval         time.Duration `long:"refresh-interval" env:"XMTPD_CONTRACTS_REFRESH_INTERVAL" description:"Refresh interval for the nodes registry" default:"60s"`
 }
 
 type DbOptions struct {
-	ReaderConnectionString string        `long:"reader-connection-string" description:"Reader connection string"`
-	WriterConnectionString string        `long:"writer-connection-string" description:"Writer connection string"                       required:"true"`
-	ReadTimeout            time.Duration `long:"read-timeout"             description:"Timeout for reading from the database"                          default:"10s"`
-	WriteTimeout           time.Duration `long:"write-timeout"            description:"Timeout for writing to the database"                            default:"10s"`
-	MaxOpenConns           int           `long:"max-open-conns"           description:"Maximum number of open connections"                             default:"80"`
-	WaitForDB              time.Duration `long:"wait-for"                 description:"wait for DB on start, up to specified duration"`
+	ReaderConnectionString string        `long:"reader-connection-string" env:"XMTPD_DB_READER_CONNECTION_STRING" description:"Reader connection string"`
+	WriterConnectionString string        `long:"writer-connection-string" env:"XMTPD_DB_WRITER_CONNECTION_STRING" description:"Writer connection string"                       required:"true"`
+	ReadTimeout            time.Duration `long:"read-timeout"             env:"XMTPD_DB_READ_TIMEOUT"             description:"Timeout for reading from the database"                          default:"10s"`
+	WriteTimeout           time.Duration `long:"write-timeout"            env:"XMTPD_DB_WRITE_TIMEOUT"            description:"Timeout for writing to the database"                            default:"10s"`
+	MaxOpenConns           int           `long:"max-open-conns"           env:"XMTPD_DB_MAX_OPEN_CONNS"           description:"Maximum number of open connections"                             default:"80"`
+	WaitForDB              time.Duration `long:"wait-for"                 env:"XMTPD_DB_WAIT_FOR"                 description:"wait for DB on start, up to specified duration"`
 }
 
 type PayerOptions struct {
-	PrivateKey string `long:"private-key" description:"Private key used to sign blockchain transactions"`
+	PrivateKey string `long:"private-key" env:"XMTPD_PAYER_PRIVATE_KEY" description:"Private key used to sign blockchain transactions"`
 }
 
 type ServerOptions struct {
-	LogLevel string `short:"l" long:"log-level"    description:"Define the logging level, supported strings are: DEBUG, INFO, WARN, ERROR, DPANIC, PANIC, FATAL, and their lower-case forms." default:"INFO"`
+	LogLevel string `short:"l" long:"log-level"    env:"XMTPD_LOG_LEVEL"    description:"Define the logging level, supported strings are: DEBUG, INFO, WARN, ERROR, DPANIC, PANIC, FATAL, and their lower-case forms." default:"INFO"`
 	//nolint:staticcheck
-	LogEncoding string `          long:"log-encoding" description:"Log encoding format. Either console or json"                                                                                  default:"console" choice:"console"`
+	LogEncoding string `          long:"log-encoding" env:"XMTPD_LOG_ENCODING" description:"Log encoding format. Either console or json"                                                                                  default:"console" choice:"console"`
 
-	SignerPrivateKey string `long:"signer-private-key" description:"Private key used to sign messages"`
+	SignerPrivateKey string `long:"signer-private-key" env:"XMTPD_SIGNER_PRIVATE_KEY" description:"Private key used to sign messages"`
 
 	API       ApiOptions       `group:"API Options"       namespace:"api"`
 	DB        DbOptions        `group:"Database Options"  namespace:"db"`

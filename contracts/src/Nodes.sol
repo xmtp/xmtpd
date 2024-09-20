140af774c09dc528374bf0f32a74eeded92e8c25
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-09-20 13:26:28 -0700

    Run smart contract registry locally

--- contracts/src/Nodes.sol
+++ contracts/src/Nodes.sol
@@ -136,11 +136,8 @@ contract Nodes is ERC721, Ownable {
         uint32 totalNodeCount = _nodeIdCounter - _startingNodeId;
         NodeWithId[] memory allNodesList = new NodeWithId[](totalNodeCount);
 
-        for (uint32 i = 0; i < totalNodeCount; i++) {
-            allNodesList[i] = NodeWithId({
-                nodeId: _startingNodeId + i,
-                node: _nodes[_startingNodeId + i]
-            });
+        for (uint32 i = _startingNodeId; i < totalNodeCount; i++) {
+            allNodesList[i] = NodeWithId({nodeId: i, node: _nodes[i]});
         }
 
         return allNodesList;

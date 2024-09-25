d066a8aa996bdcea68ed220ad68c9e6e703c26c6
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-09-24 23:11:43 -0500

    Publish messages to chain

--- pkg/testutils/api/api.go
+++ pkg/testutils/api/api.go
@@ -10,27 +10,38 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/xmtp/xmtpd/pkg/api"
 	"github.com/xmtp/xmtpd/pkg/db/queries"
+	"github.com/xmtp/xmtpd/pkg/mocks/blockchain"
 	mocks "github.com/xmtp/xmtpd/pkg/mocks/registry"
 	"github.com/xmtp/xmtpd/pkg/proto/xmtpv4/message_api"
 	"github.com/xmtp/xmtpd/pkg/registrant"
 	"github.com/xmtp/xmtpd/pkg/registry"
+	"github.com/xmtp/xmtpd/pkg/testutils"
 )
 
 func NewTestAPIServer(t *testing.T) (*api.ApiServer, *sql.DB, func()) {
 	ctx, cancel := context.WithCancel(context.Background())
-	log := NewLog(t)
-	db, _, dbCleanup := NewDB(t, ctx)
+	log := testutils.NewLog(t)
+	db, _, dbCleanup := testutils.NewDB(t, ctx)
 	privKey, err := crypto.GenerateKey()
 	require.NoError(t, err)
-	privKeyStr := "0x" + HexEncode(crypto.FromECDSA(privKey))
+	privKeyStr := "0x" + testutils.HexEncode(crypto.FromECDSA(privKey))
 	mockRegistry := mocks.NewMockNodeRegistry(t)
 	mockRegistry.EXPECT().GetNodes().Return([]registry.Node{
 		{NodeID: 1, SigningKey: &privKey.PublicKey},
 	}, nil)
 	registrant, err := registrant.NewRegistrant(ctx, queries.New(db), mockRegistry, privKeyStr)
 	require.NoError(t, err)
+	mockMessagePublsiher := blockchain.NewMockIMessagePublisher(t)
 
-	svr, err := api.NewAPIServer(ctx, db, log, 0 /*port*/, registrant, true /*enableReflection*/)
+	svr, err := api.NewAPIServer(
+		ctx,
+		db,
+		log,
+		0, /*port*/
+		registrant,
+		true, /*enableReflection*/
+		mockMessagePublsiher,
+	)
 	require.NoError(t, err)
 
 	return svr, db, func() {

203d5d23069beebf6d95e4a01353746d3b771210
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-10-22 09:43:48 -0700

    Wait for logs in blockchain publisher

---
+++ pkg/blockchain/blockchainPublisher_test.go
@@ -0,0 +1,142 @@
+package blockchain
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/xmtp/xmtpd/pkg/testutils"
+)
+
+func buildPublisher(t *testing.T) (*BlockchainPublisher, func()) {
+	ctx, cancel := context.WithCancel(context.Background())
+	logger := testutils.NewLog(t)
+	contractsOptions := testutils.GetContractsOptions(t)
+	// Set the nodes contract address to a random smart contract instead of the fixed deployment
+	contractsOptions.NodesContractAddress = testutils.DeployNodesContract(t)
+
+	signer, err := NewPrivateKeySigner(
+		testutils.GetPayerOptions(t).PrivateKey,
+		contractsOptions.ChainID,
+	)
+	require.NoError(t, err)
+
+	client, err := NewClient(ctx, contractsOptions.RpcUrl)
+	require.NoError(t, err)
+
+	publisher, err := NewBlockchainPublisher(logger, client, signer, contractsOptions)
+	require.NoError(t, err)
+
+	return publisher, func() {
+		cancel()
+		client.Close()
+	}
+}
+
+func TestPublishIdentityUpdate(t *testing.T) {
+	publisher, cleanup := buildPublisher(t)
+	defer cleanup()
+	tests := []struct {
+		name           string
+		inboxId        [32]byte
+		identityUpdate []byte
+		ctx            context.Context
+		wantErr        bool
+	}{
+		{
+			name:           "happy path",
+			inboxId:        testutils.RandomGroupID(),
+			identityUpdate: testutils.RandomBytes(100),
+			ctx:            context.Background(),
+			wantErr:        false,
+		},
+		{
+			name:           "empty update",
+			inboxId:        testutils.RandomGroupID(),
+			identityUpdate: []byte{},
+			ctx:            context.Background(),
+			wantErr:        true,
+		},
+		{
+			name:           "cancelled context",
+			inboxId:        testutils.RandomGroupID(),
+			identityUpdate: testutils.RandomBytes(100),
+			ctx:            testutils.CancelledContext(),
+			wantErr:        true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			logMessage, err := publisher.PublishIdentityUpdate(
+				tt.ctx,
+				tt.inboxId,
+				tt.identityUpdate,
+			)
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.NotNil(t, logMessage)
+			require.Equal(t, tt.inboxId, logMessage.InboxId)
+			require.Equal(t, tt.identityUpdate, logMessage.Update)
+			require.Greater(t, logMessage.SequenceId, uint64(0))
+			require.NotNil(t, logMessage.Raw.TxHash)
+		})
+	}
+}
+
+func TestPublishGroupMessage(t *testing.T) {
+	publisher, cleanup := buildPublisher(t)
+	defer cleanup()
+
+	tests := []struct {
+		name    string
+		groupID [32]byte
+		message []byte
+		ctx     context.Context
+		wantErr bool
+	}{
+		{
+			name:    "happy path",
+			groupID: testutils.RandomGroupID(),
+			message: testutils.RandomBytes(100),
+			ctx:     context.Background(),
+			wantErr: false,
+		},
+		{
+			name:    "empty message",
+			groupID: testutils.RandomGroupID(),
+			message: []byte{},
+			ctx:     context.Background(),
+			wantErr: true,
+		},
+		{
+			name:    "cancelled context",
+			groupID: testutils.RandomGroupID(),
+			message: testutils.RandomBytes(100),
+			ctx:     testutils.CancelledContext(),
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			logMessage, err := publisher.PublishGroupMessage(tt.ctx, tt.groupID, tt.message)
+			if tt.wantErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			require.NotNil(t, logMessage)
+			require.Equal(t, tt.groupID, logMessage.GroupId)
+			require.Equal(t, tt.message, logMessage.Message)
+			require.Greater(t, logMessage.SequenceId, uint64(0))
+			require.NotNil(t, logMessage.Raw.TxHash)
+		})
+	}
+}

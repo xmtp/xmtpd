b405b6b0e21a47d8327717feffeb7b3bbe976274
Borja Aranda <borja@ephemerahq.com>
2024-12-16 18:54:17 +0100

    feat: XMTP messages contracts are upgradeable and pausable

---
+++ contracts/script/DeployerNodeRegistry.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {Script, console} from "forge-std/src/Script.sol";
+import "src/Nodes.sol";
+
+contract Deployer is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        new Nodes();
+
+        vm.broadcast();
+    }
+}

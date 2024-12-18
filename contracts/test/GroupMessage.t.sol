d2a93dd696ed3e72c4bc3a50aa1ddbb19639ab95
Borja Aranda <borja@ephemerahq.com>
2024-12-18 19:19:26 +0100

    feat: add tests and scripts

--- contracts/test/GroupMessage.t.sol
+++ contracts/test/GroupMessage.t.sol
@@ -192,4 +192,97 @@ contract GroupMessagesTest is Test, GroupMessages, Utils {
         emit GroupMessages.MessageSent(ID, message, 2);
         groupMessages.addMessage(ID, message);
     }
+
+    function testAddMessageInvalid() public {
+        bytes memory message = new bytes(77);
+        for (uint256 i = 0; i < message.length; i++) {
+            message[i] = bytes1(uint8(i % 256));
+    }
+
+        vm.expectRevert(GroupMessages.InvalidMessage.selector);
+        groupMessages.addMessage(GROUP_ID, message);
+    }
+
+    function testInvalidReinitialization() public {
+        vm.expectRevert(Initializable.InvalidInitialization.selector);
+        groupMessages.initialize(admin);
+    }
+
+    function testPauseUnpause() public {
+        groupMessages.pause();
+
+        vm.prank(unauthorized);
+        vm.expectRevert();
+        groupMessages.unpause();
+
+        groupMessages.unpause();
+
+        vm.prank(unauthorized);
+        vm.expectRevert();
+        groupMessages.pause();
+    }
+
+    function testRoles() public {
+        groupMessages.grantRole(DEFAULT_ADMIN_ROLE, unauthorized);
+
+        vm.startPrank(unauthorized);
+        groupMessages.pause();
+        groupMessages.unpause();
+        vm.stopPrank();
+
+        groupMessages.revokeRole(DEFAULT_ADMIN_ROLE, unauthorized);
+
+        vm.prank(unauthorized);
+        vm.expectRevert(revertRoleData(unauthorized));
+        groupMessages.pause();
+
+        groupMessages.renounceRole(DEFAULT_ADMIN_ROLE, admin);
+        vm.expectRevert(revertRoleData(admin));
+        groupMessages.pause();
+    }
+
+    function testUpgradeImplementation() public {
+        GroupMessages newGroupMessagesImpl = new GroupMessages();
+        address newImplAddress = address(newGroupMessagesImpl);
+        address oldImplAddress = address(groupMessagesImpl);
+        
+        bytes memory message = new bytes(78);
+        for (uint256 i = 0; i < message.length; i++) {
+            message[i] = bytes1(uint8(i % 256));
+        }
+
+        // Retrieve the implementation address directly from the proxy storage.
+        bytes32 rawImplAddress = vm.load(address(groupMessages), EIP1967_IMPL_SLOT);
+        address implementationAddress = address(uint160(uint256(rawImplAddress)));
+        assertEq(implementationAddress, oldImplAddress);
+
+        // Initialize sequenceId to 1. The state should be preserved between upgrades.
+        vm.expectEmit(address(groupMessages));
+        emit GroupMessages.MessageSent(GROUP_ID, message, 1);
+        groupMessages.addMessage(GROUP_ID, message);
+
+        // Unauthorized upgrade attempts should revert.
+        vm.prank(unauthorized);
+        vm.expectRevert(revertRoleData(unauthorized));
+        groupMessages.upgradeToAndCall(address(newGroupMessagesImpl), "");
+
+        // Authorized upgrade should succeed and emit UpgradeAuthorized event.
+        vm.expectEmit(address(groupMessages));
+        emit GroupMessages.UpgradeAuthorized(address(this), address(newGroupMessagesImpl));
+        groupMessages.upgradeToAndCall(address(newGroupMessagesImpl), "");
+
+        // Retrieve the new implementation address directly from the proxy storage.
+        rawImplAddress = vm.load(address(groupMessages), EIP1967_IMPL_SLOT);
+        implementationAddress = address(uint160(uint256(rawImplAddress)));
+        assertEq(implementationAddress, newImplAddress);
+
+        // Next sequenceId should be 2.
+        vm.expectEmit(address(groupMessages));
+        emit GroupMessages.MessageSent(GROUP_ID, message, 2);
+        groupMessages.addMessage(GROUP_ID, message);
+    }
+
+    function revertRoleData(address _user) public pure returns (bytes memory) {
+        return abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, _user, DEFAULT_ADMIN_ROLE);
+    }
 }

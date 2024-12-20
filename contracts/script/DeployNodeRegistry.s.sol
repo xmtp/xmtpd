08ce2ec217254a6e50db981fb47b298d4d0c2739
Borja Aranda <borja@ephemerahq.com>
2024-12-20 17:56:18 +0100

    updates all licenses to MIT

--- contracts/script/DeployNodeRegistry.s.sol
+++ contracts/script/DeployNodeRegistry.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
 import {Script, console} from "forge-std/src/Script.sol";
--- contracts/test/Nodes.sol
+++ contracts/test/Nodes.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
 import {Test, console} from "forge-std/src/Test.sol";

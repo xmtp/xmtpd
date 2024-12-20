7d6fa23d3b5727f680ffb9b6f105b369f4d63250
Borja Aranda <borja@ephemerahq.com>
2024-12-20 13:47:15 +0100

    make CI happy

--- contracts/script/utils/Environment.sol
+++ contracts/script/utils/Environment.sol
@@ -4,6 +4,6 @@ pragma solidity 0.8.28;
 import "forge-std/src/Script.sol";
 
 contract Environment is Script {
-    string public constant XMTP_GROUP_MESSAGES_OUTPUT_JSON = "group_messages_deployment";
-    string public constant XMTP_IDENTITY_UPDATES_OUTPUT_JSON = "identity_updates_deployment";
+    string public constant XMTP_GROUP_MESSAGES_OUTPUT_JSON = "GroupMessages";
+    string public constant XMTP_IDENTITY_UPDATES_OUTPUT_JSON = "IdentityUpdates";
 }

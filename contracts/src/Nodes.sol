2a5a40cd652fd53dc1b5adef8b9b15a5595faafc
Borja Aranda <borja@ephemerahq.com>
2025-02-20 13:51:36 +0100

    Add Nodes and INodes

--- contracts/src/Nodes.sol
+++ contracts/src/Nodes.sol
@@ -3,158 +3,201 @@ pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "./interfaces/INodes.sol";
 
-/**
- * A NFT contract for XMTP Node Operators.
- *
- * The deployer of this contract is responsible for minting NFTs and assigning them to node operators.
- *
- * All nodes on the network periodically check this contract to determine which nodes they should connect to.
- */
-contract Nodes is ERC721, Ownable {
-    constructor() ERC721("XMTP Node Operator", "XMTP") Ownable(msg.sender) {}
 
+ /// @title XMTP Node Registry.
+ /// @notice This contract is responsible for minting NFTs and assigning them to node operators.
+ /// Each node is minted as an NFT with a unique ID (starting at 100 and increasing by 100 with each new node).
+ /// In addition to the standard ERC721 functionality, the contract supports node-specific features,
+ /// including node property updates.
+ /// 
+ /// @dev All nodes on the network periodically check this contract to determine which nodes they should connect to.
+ /// The contract owner is responsible for:
+ /// - minting and transferring NFTs to node operators.
+ /// - updating the node operator's HTTP address and MTLS certificate.
+ /// - updating the node operator's minimum monthly fee.
+ /// - updating the node operator's API enabled flag.
+contract Nodes is ERC721, INodes, Ownable {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    /// @dev The maximum commission percentage that the node operator can receive.
+    uint256 public constant MAX_BPS = 10000;
+
+    /// @dev The increment for node IDs.
     uint32 private constant NODE_INCREMENT = 100;
-    // uint32 counter so that we cannot create more than max IDs
-    // The ERC721 standard expects the tokenID to be uint256 for standard methods unfortunately
+
+    /// @dev Max number of active nodes.
+    // slither-disable-next-line constable-states
+    uint8 public maxActiveNodes = 20;
+
+    /// @dev The counter for n max IDs.
+    // The ERC721 standard expects the tokenID to be uint256 for standard methods unfortunately.
+    // slither-disable-next-line constable-states
     uint32 private _nodeCounter = 0;
 
-    // A node, as stored in the internal mapping
-    struct Node {
-        bytes signingKeyPub;
-        string httpAddress;
-        bool isHealthy;
-        bool isActive;
-    }
-
-    struct NodeWithId {
-        uint32 nodeId;
-        Node node;
-    }
-
-    event NodeCreated(uint256 nodeId, Node node);
-    event NodeUpdated(uint256 nodeId, Node node);
-    event NodeActivated(uint256 nodeId);
-    event NodeDeactivated(uint256 nodeId);
-
-    // Mapping of token ID to Node
+    /// @dev Mapping of token ID to Node.
     mapping(uint256 => Node) private _nodes;
 
-    /**
-     * Mint a new node NFT and store the metadata in the smart contract
-     */
-    function addNode(address to, bytes calldata signingKeyPub, string calldata httpAddress)
-        public
+    /// @dev Active Node Operators IDs set.
+    EnumerableSet.UintSet private _activeNodes;
+
+    /// @notice The commission percentage that the node operator receives.
+    /// @dev This is stored in basis points (1/100th of a percent).
+    /// Example: 1% = 100bps, 10% = 1000bps, 100% = 10000bps.
+    /// Comission is calculated as (nodeOperatorCommissionPercent * nodeOperatorFee) / MAX_BPS.
+    // slither-disable-next-line constable-states
+    uint256 public nodeOperatorCommissionPercent;
+
+    constructor() ERC721("XMTP Node Operator", "XMTP") Ownable(msg.sender) {}
+
+    /// @inheritdoc INodes
+    function addNode(address to, bytes calldata signingKeyPub, string calldata httpAddress, uint256 minMonthlyFee)
+        external
         onlyOwner
-        returns (uint32)
+        returns (uint256)
     {
+        require(to != address(0), InvalidAddress());
+        require(signingKeyPub.length > 0, InvalidSigningKey());
+        require(bytes(httpAddress).length > 0, InvalidHttpAddress());
+
         // the first node starts with 100
         _nodeCounter++;
         uint32 nodeId = _nodeCounter * NODE_INCREMENT;
         _mint(to, nodeId);
-        _nodes[nodeId] = Node(signingKeyPub, httpAddress, true, false);
-        emit NodeCreated(nodeId, _nodes[nodeId]);
+        _nodes[nodeId] = Node(signingKeyPub, httpAddress, false, false, false, minMonthlyFee);
+        emit NodeAdded(nodeId, to, signingKeyPub, httpAddress, minMonthlyFee);
         return nodeId;
     }
 
-    /**
-     * Override the built in transferFrom function to block NFT owners from transferring
-     * node ownership.
-     *
-     * NFT owners are only allowed to update their HTTP address and MTLS cert.
-     */
-    function transferFrom(address from, address to, uint256 tokenId) public override {
-        require(_msgSender() == owner(), "Only the contract owner can transfer Node ownership");
-        super.transferFrom(from, to, tokenId);
+    /// @notice Transfers node ownership from one address to another
+    /// @dev Only the contract owner may call this. Automatically deactivates the node
+    /// @param from The current owner address
+    /// @param to The new owner address
+    /// @param nodeId The ID of the node being transferred
+    function transferFrom(address from, address to, uint256 nodeId) 
+        public 
+        override(ERC721, IERC721) 
+        onlyOwner 
+    {
+        // Deactivate node before transfer
+        if (_activeNodes.contains(nodeId)) {
+            _activeNodes.remove(nodeId);
+            _nodes[nodeId].isActive = false;
+            emit NodeActivateUpdated(nodeId, false);
+        }
+        super.transferFrom(from, to, nodeId);
+        emit NodeTransferred(nodeId, from, to);
     }
 
-    /**
-     * Allow a NFT holder to update the HTTP address of their node
-     */
-    function updateHttpAddress(uint256 tokenId, string calldata httpAddress) public {
-        require(_msgSender() == ownerOf(tokenId), "Only the owner of the Node NFT can update its http address");
-        _nodes[tokenId].httpAddress = httpAddress;
-        emit NodeUpdated(tokenId, _nodes[tokenId]);
+    /// @inheritdoc INodes
+    function updateHttpAddress(uint256 nodeId, string calldata httpAddress) external onlyOwner {
+        require(_nodeExists(nodeId), NodeDoesNotExist());
+        require(bytes(httpAddress).length > 0, InvalidHttpAddress());
+        _nodes[nodeId].httpAddress = httpAddress;
+        emit HttpAddressUpdated(nodeId, httpAddress);
     }
 
-    /**
-     * The contract owner may update the health status of the node.
-     *
-     * No one else is allowed to call this function.
-     */
-    function updateHealth(uint256 tokenId, bool isHealthy) public onlyOwner {
-        // Make sure that the token exists
-        _requireOwned(tokenId);
-        _nodes[tokenId].isHealthy = isHealthy;
-        emit NodeUpdated(tokenId, _nodes[tokenId]);
+    /// @inheritdoc INodes
+    function updateIsReplicationEnabled(uint256 nodeId, bool isReplicationEnabled) external onlyOwner {
+        require(_nodeExists(nodeId), NodeDoesNotExist());
+        _nodes[nodeId].isReplicationEnabled = isReplicationEnabled;
+        emit ReplicationEnabledUpdated(nodeId, isReplicationEnabled);
     }
 
-    function updateActive(uint256 tokenId, bool isActive) public onlyOwner {
-        _requireOwned(tokenId);
-        _nodes[tokenId].isActive = isActive;
+    /// @inheritdoc INodes
+    function updateMinMonthlyFee(uint256 nodeId, uint256 minMonthlyFee) external onlyOwner {
+        require(_nodeExists(nodeId), NodeDoesNotExist());
+        _nodes[nodeId].minMonthlyFee = minMonthlyFee;
+        emit MinMonthlyFeeUpdated(nodeId, minMonthlyFee);
+    }
+
+    /// @inheritdoc INodes
+    function updateActive(uint256 nodeId, bool isActive) public onlyOwner {
+        require(_nodeExists(nodeId), NodeDoesNotExist());
+        require(_activeNodes.length() < maxActiveNodes, MaxActiveNodesReached());
         if (isActive) {
-            emit NodeActivated(tokenId);
+            require(_activeNodes.add(nodeId), NodeAlreadyActive());
         } else {
-            emit NodeDeactivated(tokenId);
+            require(_activeNodes.remove(nodeId), NodeAlreadyInactive());
+        }
+        _nodes[nodeId].isActive = isActive;
+        emit NodeActivateUpdated(nodeId, isActive);
+    }
+
+    /// @inheritdoc INodes
+    function batchUpdateActive(uint256[] calldata nodeIds, bool[] calldata isActive) 
+        external 
+        onlyOwner 
+    {
+        require(nodeIds.length == isActive.length);
+        for (uint256 i = 0; i < nodeIds.length; i++) {
+            updateActive(nodeIds[i], isActive[i]);
         }
     }
 
-    /**
-     * Get a list of healthy nodes with their ID and metadata
-     */
-    function healthyNodes() public view returns (NodeWithId[] memory) {
-        uint256 healthyCount = 0;
-
-        // First, count the number of healthy nodes
-        for (uint256 i = 0; i < _nodeCounter; i++) {
-            uint256 nodeId = NODE_INCREMENT * (i + 1);
-            if (_nodeExists(nodeId) && _nodes[nodeId].isHealthy) {
-                healthyCount++;
-            }
-        }
-
-        // Create an array to store healthy nodes
-        NodeWithId[] memory healthyNodesList = new NodeWithId[](healthyCount);
-        uint256 currentIndex = 0;
-
-        // Populate the array with healthy nodes
-        for (uint32 i = 0; i < _nodeCounter; i++) {
-            uint32 nodeId = NODE_INCREMENT * (i + 1);
-            if (_nodeExists(nodeId) && _nodes[nodeId].isHealthy) {
-                healthyNodesList[currentIndex] = NodeWithId({nodeId: nodeId, node: _nodes[nodeId]});
-                currentIndex++;
-            }
-        }
-
-        return healthyNodesList;
+    /// @inheritdoc INodes
+    function updateMaxActiveNodes(uint8 newMaxActiveNodes) external onlyOwner {
+        maxActiveNodes = newMaxActiveNodes;
+        emit MaxActiveNodesUpdated(newMaxActiveNodes);
     }
 
-    /**
-     * Get all nodes regardless of their health status
-     */
+    /// @inheritdoc INodes
+    function updateNodeOperatorCommissionPercent(uint256 newCommissionPercent) external onlyOwner {
+        require(newCommissionPercent <= MAX_BPS, InvalidCommissionPercent());
+        nodeOperatorCommissionPercent = newCommissionPercent;
+        emit NodeOperatorCommissionPercentUpdated(newCommissionPercent);
+    }   
+
+    /// @inheritdoc INodes
+    function updateIsApiEnabled(uint256 nodeId) external {
+        require(_ownerOf(nodeId) == msg.sender, Unauthorized());
+        _nodes[nodeId].isApiEnabled = !_nodes[nodeId].isApiEnabled;
+        emit ApiEnabledUpdated(nodeId, _nodes[nodeId].isApiEnabled);
+    }
+
+    /// @inheritdoc INodes
     function allNodes() public view returns (NodeWithId[] memory) {
         NodeWithId[] memory allNodesList = new NodeWithId[](_nodeCounter);
-
         for (uint32 i = 0; i < _nodeCounter; i++) {
             uint32 nodeId = NODE_INCREMENT * (i + 1);
             if (_nodeExists(nodeId)) {
                 allNodesList[i] = NodeWithId({nodeId: nodeId, node: _nodes[nodeId]});
             }
         }
-
         return allNodesList;
     }
 
-    /**
-     * Get a node's metadata by ID
-     */
-    function getNode(uint256 tokenId) public view returns (Node memory) {
-        _requireOwned(tokenId);
-        return _nodes[tokenId];
+    /// @inheritdoc INodes
+    function getNode(uint256 nodeId) public view returns (Node memory) {
+        require(_nodeExists(nodeId), NodeDoesNotExist());
+        return _nodes[nodeId];
     }
 
-    function _nodeExists(uint256 tokenId) private view returns (bool) {
-        address owner = _ownerOf(tokenId);
-        return owner != address(0);
+    /// @inheritdoc INodes
+    function getActiveNodes() external view returns (Node[] memory activeNodes) {
+        activeNodes = new Node[](_activeNodes.length());
+        for (uint32 i = 0; i < _activeNodes.length(); i++) {
+            activeNodes[i] = _nodes[_activeNodes.at(i)];
+        }
+        return activeNodes;
+    }
+
+    /// @inheritdoc INodes
+    function getActiveNodesIDs() external view returns (uint256[] memory activeNodesIDs) {
+        return _activeNodes.values();
+    }
+
+    /// @inheritdoc INodes
+    function nodeIsActive(uint256 nodeId) external view returns (bool) {
+        return _activeNodes.contains(nodeId);
+    }
+
+    /// @dev Checks if a node exists.
+    /// @param nodeId The ID of the node to check.
+    /// @return True if the node exists, false otherwise.
+    function _nodeExists(uint256 nodeId) private view returns (bool) {
+        return _ownerOf(nodeId) != address(0);
     }
 }
--- contracts/src/interfaces/INodes.sol
+++ contracts/src/interfaces/INodes.sol
@@ -1,36 +1,234 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-/**
- * Interface for the XMTP Node Operators NFT contract.
- */
-interface INodes {
+// Import the standard ERC721 interface.
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+/// @title INodes
+/// @notice This interface defines the ERC721-based registry for “nodes” in the system.
+/// Each node is minted as an NFT with a unique ID (starting at 100 and increasing by 100 with each new node).
+/// In addition to the standard ERC721 functionality, the contract supports node-specific features,
+/// including node property updates.
+///
+/// Note: The staking contract reference has been removed per updated requirements.
+interface INodes is IERC721 {
+
+    /// @notice Error thrown when an unauthorized address attempts to call a function.
+    error Unauthorized();
+
+    /// @notice Error thrown when an invalid address is provided.
+    error InvalidAddress();
+
+    /// @notice Error thrown when an invalid signing key is provided.
+    error InvalidSigningKey();
+
+    /// @notice Error thrown when an invalid HTTP address is provided.
+    error InvalidHttpAddress();
+
+    /// @notice Error thrown when a node does not exist.
+    error NodeDoesNotExist();
+
+    /// @notice Error thrown when a node is already active.
+    error NodeAlreadyActive();
+
+    /// @notice Error thrown when a node is already inactive.
+    error NodeAlreadyInactive();
+
+    /// @notice Error thrown when an invalid commission percentage is provided.
+    error InvalidCommissionPercent();
+
+    /// @notice Error thrown when the maximum number of active nodes is reached.
+    error MaxActiveNodesReached();
+
+    /// @notice Struct representing a node in the registry.
+    /// @param signingKeyPub The public key used for node signing/verification.
+    /// @param httpAddress The HTTP endpoint address for the node.
+    /// @param isReplicationEnabled A flag indicating whether the node supports replication.
+    /// @param isApiEnabled A flag indicating whether the node has its API enabled.
+    /// @param minMonthlyFee The minimum monthly fee collected by the node operator.
     struct Node {
         bytes signingKeyPub;
         string httpAddress;
-        bool isHealthy;
+        bool isReplicationEnabled;
+        bool isApiEnabled;
         bool isActive;
+        uint256 minMonthlyFee;
     }
 
+    /// @notice Struct representing a node with its ID
+    /// @param nodeId The unique identifier for the node
+    /// @param node The node struct
     struct NodeWithId {
-        uint32 nodeId;
+        uint256 nodeId;
         Node node;
     }
 
-    event NodeCreated(uint256 nodeId, Node node);
-    event NodeUpdated(uint256 nodeId, Node node);
-    event NodeActivated(uint256 nodeId);
-    event NodeDeactivated(uint256 nodeId);
+    // ***************************************************************
+    // *                        EVENTS                             *
+    // ***************************************************************
 
-    function addNode(address to, bytes calldata signingKeyPub, string calldata httpAddress) external returns (uint32);
-    function transferFrom(address from, address to, uint256 tokenId) external;
-    function updateHttpAddress(uint256 tokenId, string calldata httpAddress) external;
-    function updateHealth(uint256 tokenId, bool isHealthy) external;
-    function updateActive(uint256 tokenId, bool isActive) external;
-    function healthyNodes() external view returns (NodeWithId[] memory);
+    /// @notice Emitted when a new node is added and its NFT minted.
+    /// @param nodeId The unique identifier for the node (starts at 100, increments by 100).
+    /// @param owner The address that receives the new node NFT.
+    /// @param signingKeyPub The node’s signing key public value.
+    /// @param httpAddress The node’s HTTP endpoint.
+    /// @param minMonthlyFee The minimum monthly fee for the node.
+    event NodeAdded(
+        uint256 indexed nodeId,
+        address indexed owner,
+        bytes signingKeyPub,
+        string httpAddress,
+        uint256 minMonthlyFee
+    );
+
+    /// @notice Emitted when the HTTP address for a node is updated.
+    /// @param nodeId The identifier of the node.
+    /// @param newHttpAddress The new HTTP address.
+    event HttpAddressUpdated(
+        uint256 indexed nodeId,
+        string newHttpAddress
+    );
+
+    /// @notice Emitted when the replication flag for a node is updated.
+    /// @param nodeId The identifier of the node.
+    /// @param isReplicationEnabled The updated replication flag.
+    event ReplicationEnabledUpdated(
+        uint256 indexed nodeId,
+        bool isReplicationEnabled
+    );
+
+    /// @notice Emitted when the minimum monthly fee for a node is updated.
+    /// @param nodeId The identifier of the node.
+    /// @param minMonthlyFee The updated minimum fee.
+    event MinMonthlyFeeUpdated(
+        uint256 indexed nodeId,
+        uint256 minMonthlyFee
+    );
+
+    /// @notice Emitted when the API enabled flag for a node is toggled.
+    /// @param nodeId The identifier of the node.
+    /// @param isApiEnabled The updated API enabled flag.
+    event ApiEnabledUpdated(
+        uint256 indexed nodeId,
+        bool isApiEnabled
+    );
+
+    /// @notice Emitted when the node operator commission percent is updated.
+    /// @param newCommissionPercent The new commission percentage.
+    event NodeOperatorCommissionPercentUpdated(
+        uint256 newCommissionPercent
+    );
+
+    /// @notice Emitted when the active status of a node is updated.
+    /// @param nodeId The identifier of the node.
+    /// @param isActive The updated active status.
+    event NodeActivateUpdated(
+        uint256 indexed nodeId,
+        bool isActive);
+
+    /// @notice Emitted when a node is transferred
+    event NodeTransferred(uint256 indexed nodeId, address indexed from, address indexed to);
+
+    /// @notice Emitted when the maximum number of active nodes is updated.
+    /// @param newMaxActiveNodes The new maximum number of active nodes.
+    event MaxActiveNodesUpdated(uint8 newMaxActiveNodes);
+
+    // ***************************************************************
+    // *                OWNER-ONLY FUNCTIONS                       *
+    // ***************************************************************
+
+    /// @notice Adds a new node to the registry and mints its corresponding ERC721 token.
+    /// @dev Only the contract owner may call this. Node IDs start at 100 and increase by 100 for each new node.
+    /// @param to The address that will own the new node NFT.
+    /// @param signingKeyPub The public signing key for the node.
+    /// @param httpAddress The node’s HTTP address.
+    /// @param minMonthlyFee The minimum monthly fee that the node operator collects.
+    /// @return nodeId The unique identifier of the newly added node.
+    function addNode(
+        address to,
+        bytes calldata signingKeyPub,
+        string calldata httpAddress,
+        uint256 minMonthlyFee
+    ) external returns (uint256 nodeId);
+
+    /// @notice Updates the HTTP address of an existing node.
+    /// @dev Only the contract owner may call this.
+    /// @param nodeId The unique identifier of the node.
+    /// @param httpAddress The new HTTP address.
+    function updateHttpAddress(uint256 nodeId, string calldata httpAddress) external;
+
+    /// @notice Updates whether replication is enabled for a node.
+    /// @dev Only the contract owner may call this.
+    /// @param nodeId The unique identifier of the node.
+    /// @param isReplicationEnabled A boolean indicating if replication should be enabled.
+    function updateIsReplicationEnabled(uint256 nodeId, bool isReplicationEnabled) external;
+
+    /// @notice Updates the minimum monthly fee for a node.
+    /// @dev Only the contract owner may call this.
+    /// @param nodeId The unique identifier of the node.
+    /// @param minMonthlyFee The new minimum monthly fee.
+    function updateMinMonthlyFee(uint256 nodeId, uint256 minMonthlyFee) external;
+
+    /// @notice Updates the commission percentage that the node operator receives.
+    /// @dev Only the contract owner may call this.
+    /// @param newCommissionPercent The new commission percentage.
+    function updateNodeOperatorCommissionPercent(uint256 newCommissionPercent) external;
+
+    /// @notice Updates the active status of a node.
+    /// @dev Only the contract owner may call this.
+    /// @param nodeId The unique identifier of the node.
+    /// @param isActive The new active status.
+    function updateActive(uint256 nodeId, bool isActive) external;
+
+    /// @notice Updates multiple nodes active status at once.
+    /// @dev Only the contract owner may call this.
+    /// @param nodeIds Array of node IDs.
+    /// @param isActive Array of active status flags.
+    function batchUpdateActive(uint256[] calldata nodeIds, bool[] calldata isActive) external;
+
+    /// @notice Updates the maximum number of active nodes.
+    /// @dev Only the contract owner may call this.
+    /// @param newMaxActiveNodes The new maximum number of active nodes.
+    function updateMaxActiveNodes(uint8 newMaxActiveNodes) external;
+
+    // ***************************************************************
+    // *                NODE OWNER FUNCTION                        *
+    // ***************************************************************
+
+    /// @notice Toggles the API enabled flag for the node owned by the caller.
+    /// @dev Only the owner of the node NFT may call this.
+    /// @param nodeId The unique identifier of the node.
+    function updateIsApiEnabled(uint256 nodeId) external;
+
+    // ***************************************************************
+    // *                     GETTER FUNCTIONS                      *
+    // ***************************************************************
+
+    /// @notice Gets all nodes regardless of their health status
+    /// @return allNodesList An array of all nodes in the registry
     function allNodes() external view returns (NodeWithId[] memory);
-    function getNode(uint256 tokenId) external view returns (Node memory);
 
-    // Include required ERC721 interface functions
-    function ownerOf(uint256 tokenId) external view returns (address);
-} 
+    /// @notice Retrieves the details of a given node.
+    /// @param nodeId The unique identifier of the node.
+    /// @return The Node struct containing the node's details.
+    function getNode(uint256 nodeId) external view returns (Node memory);
+
+    /// @notice Retrieves the current node operator commission percentage.
+    /// @return The commission percentage.
+    function nodeOperatorCommissionPercent() external view returns (uint256);
+
+    /// @notice Retrieves a list of active nodes.
+    /// @dev Active nodes are those with `isReplicationEnabled` set to true.
+    /// @return activeNodes An array of Node structs representing active nodes.
+    function getActiveNodes() external view returns (Node[] memory activeNodes);
+
+    /// @notice Retrieves a list of active nodes IDs.
+    /// @dev Active nodes are those with `isReplicationEnabled` set to true.
+    /// @return activeNodesIDs An array of node IDs representing active nodes.
+    function getActiveNodesIDs() external view returns (uint256[] memory activeNodesIDs);
+
+    /// @notice Retrieves if a node is active.
+    /// @param nodeId The ID of the node NFT.
+    /// @return isActive A boolean indicating if the node is active.
+    function nodeIsActive(uint256 nodeId) external view returns (bool);
+}

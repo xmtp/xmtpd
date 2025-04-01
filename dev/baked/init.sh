017b581c8209d78f2de8c898f600f3640ff6db40
Borja Aranda <borja@ephemerahq.com>
2025-04-01 16:03:05 +0200

    Baked container image

---
+++ dev/baked/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+# Work always from the root directory
+script_dir=$(dirname "$(realpath "$0")")
+repo_root=$(realpath "${script_dir}/../../")
+cd "${repo_root}"
+
+source dev/local.env
+
+echo "Loading anvil state"
+anvil --state "${repo_root}/deployments/anvil_localnet/anvil-state.json" &>/dev/null &
+
+echo "Registering local node 1"
+dev/cli/register-local-node-1
+
+echo "Registering local node 2"
+dev/cli/register-local-node-2
+
+echo "Stopping anvil"
+pkill -f anvil && sleep 5

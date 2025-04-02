3528e75c68b9aa2ecee62050077354af538d982f
Borja Aranda <borja@ephemerahq.com>
2025-04-02 23:26:38 +0200

    Update baked dockerfile

--- dev/baked/bake.sh
+++ dev/baked/bake.sh
@@ -6,10 +6,12 @@ script_dir=$(dirname "$(realpath "$0")")
 repo_root=$(realpath "${script_dir}/../../")
 cd "${repo_root}"
 
+export RPC_URL=http://localhost:8545
+
 source dev/local.env
 
 echo "Loading anvil state"
-anvil --state "${repo_root}/deployments/anvil_localnet/anvil-state.json" &>/dev/null &
+anvil --state "${1}" &>/dev/null &
 ANVIL_PID=$!
 
 echo "Registering local node 1"

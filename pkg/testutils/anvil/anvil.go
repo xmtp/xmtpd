0077f1e5588276f59bbc091dd799d9e5e27a34af
Martin Kysel <code@martinkysel.com>
2025-04-08 11:03:36 -0400

    depend on external anvil

--- pkg/testutils/anvil/anvil.go
+++ pkg/testutils/anvil/anvil.go
@@ -2,16 +2,12 @@ package anvil
 
 import (
 	"context"
-	"fmt"
 	"math/big"
-	"os"
-	"os/exec"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/xmtp/xmtpd/pkg/blockchain"
-	networkTestUtils "github.com/xmtp/xmtpd/pkg/testutils/network"
 )
 
 func waitForAnvil(t *testing.T, url string) {
@@ -46,30 +42,34 @@ func waitForAnvil(t *testing.T, url string) {
 
 // Start an ephemeral anvil instance and return the address and a cleanup function
 func StartAnvil(t *testing.T, showLogs bool) (string, func()) {
-	port := networkTestUtils.FindFreePort(t)
+	//port := networkTestUtils.FindFreePort(t)
+	//
+	//// we need mixed mining to work around https://github.com/xmtp/xmtpd/issues/643
+	//cmd := exec.Command(
+	//	"anvil",
+	//	"--port",
+	//	fmt.Sprintf("%d", port),
+	//	"--mixed-mining",
+	//	"--block-time",
+	//	"1",
+	//)
+	//if showLogs {
+	//	// (Optional) You can capture stdout/stderr for logs:
+	//	cmd.Stdout = os.Stdout
+	//	cmd.Stderr = os.Stderr
+	//}
+	//
+	//err := cmd.Start()
+	//require.NoError(t, err)
+	//url := fmt.Sprintf("http://localhost:%d", port)
+	//waitForAnvil(t, url)
+	//
+	//return url, func() {
+	//	_ = cmd.Process.Kill()
+	//	_ = cmd.Wait()
+	//}
 
-	// we need mixed mining to work around https://github.com/xmtp/xmtpd/issues/643
-	cmd := exec.Command(
-		"anvil",
-		"--port",
-		fmt.Sprintf("%d", port),
-		"--mixed-mining",
-		"--block-time",
-		"1",
-	)
-	if showLogs {
-		// (Optional) You can capture stdout/stderr for logs:
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-	}
+	return "http://localhost:8545", func() {
 
-	err := cmd.Start()
-	require.NoError(t, err)
-	url := fmt.Sprintf("http://localhost:%d", port)
-	waitForAnvil(t, url)
-
-	return url, func() {
-		_ = cmd.Process.Kill()
-		_ = cmd.Wait()
 	}
 }

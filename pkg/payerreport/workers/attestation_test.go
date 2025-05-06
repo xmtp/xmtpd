88ea2fcf7fa1db414ee8f6079efd8c6a995325fd
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2025-05-06 19:54:08 -0400

    Add methods to attestation worker

---
+++ pkg/payerreport/workers/attestation_test.go
@@ -0,0 +1,109 @@
+package workers
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	payerreportMocks "github.com/xmtp/xmtpd/pkg/mocks/payerreport"
+	registrantMocks "github.com/xmtp/xmtpd/pkg/mocks/registrant"
+	"github.com/xmtp/xmtpd/pkg/payerreport"
+	"github.com/xmtp/xmtpd/pkg/testutils"
+)
+
+func testAttestationWorker(
+	t *testing.T,
+	pollInterval time.Duration,
+) (*attestationWorker, *payerreport.Store, *registrantMocks.MockIRegistrant, *payerreportMocks.MockPayerReportVerifier) {
+	log := testutils.NewLog(t)
+	ctx := t.Context()
+	db, _ := testutils.NewDB(t, ctx)
+	store := payerreport.NewStore(db, log)
+	mockRegistrant := registrantMocks.NewMockIRegistrant(t)
+	verifier := payerreportMocks.NewMockPayerReportVerifier(t)
+	worker := NewAttestationWorker(ctx, log, mockRegistrant, verifier, store, pollInterval)
+
+	return worker, store, mockRegistrant, verifier
+}
+
+func storeReport(
+	t *testing.T,
+	store *payerreport.Store,
+	report *payerreport.PayerReport,
+) *payerreport.PayerReportWithStatus {
+	id, err := store.StoreReport(t.Context(), report)
+	require.NoError(t, err)
+	reportWithStatus, err := store.FetchReport(t.Context(), id)
+	require.NoError(t, err)
+
+	return reportWithStatus
+}
+
+func setReportAttestationStatus(
+	t *testing.T,
+	store payerreport.IPayerReportStore,
+	id payerreport.ReportID,
+	attestationStatus payerreport.AttestationStatus,
+) {
+	require.NoError(
+		t,
+		store.SetReportAttestationStatus(
+			t.Context(),
+			id,
+			[]payerreport.AttestationStatus{payerreport.AttestationPending},
+			attestationStatus,
+		),
+	)
+}
+
+func TestFindReport(t *testing.T) {
+	worker, store, _, _ := testAttestationWorker(t, time.Second)
+
+	report, err := payerreport.NewPayerReport(payerreport.NewPayerReportParams{
+		OriginatorNodeID: 1,
+		StartSequenceID:  1,
+		EndSequenceID:    10,
+	})
+	require.NoError(t, err)
+	storedReport := storeReport(t, store, &report.PayerReport)
+
+	reports, err := worker.findReportsNeedingAttestation()
+	require.NoError(t, err)
+	require.Len(t, reports, 1)
+	require.Equal(t, storedReport.ID, reports[0].ID)
+
+	setReportAttestationStatus(t, store, storedReport.ID, payerreport.AttestationApproved)
+
+	reports, err = worker.findReportsNeedingAttestation()
+	require.NoError(t, err)
+	require.Len(t, reports, 0)
+}
+
+func TestAttestFirstReport(t *testing.T) {
+	worker, store, _, mockVerifier := testAttestationWorker(t, time.Second)
+
+	report, err := payerreport.NewPayerReport(payerreport.NewPayerReportParams{
+		OriginatorNodeID: 1,
+		StartSequenceID:  0,
+		EndSequenceID:    10,
+		NodeIDs:          []uint32{1},
+	})
+	storedReport := storeReport(t, store, &report.PayerReport)
+	require.NoError(t, err)
+
+	mockVerifier.EXPECT().
+		IsValidReport(mock.Anything, (*payerreport.PayerReport)(nil), &report.PayerReport).
+		Return(true, nil)
+
+	err = worker.attestReport(storedReport)
+	require.NoError(t, err)
+
+	fromDB, err := store.FetchReport(t.Context(), storedReport.ID)
+	require.NoError(t, err)
+	require.Equal(
+		t,
+		payerreport.AttestationStatus(payerreport.AttestationApproved),
+		fromDB.AttestationStatus,
+	)
+}

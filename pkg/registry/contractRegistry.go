e119581c906ffaf2a1405c0285869150aeaede0b
Martin Kysel <code@martinkysel.com>
2025-05-07 11:49:19 +0100

    Clarify error to help with support

--- pkg/registry/contractRegistry.go
+++ pkg/registry/contractRegistry.go
@@ -3,6 +3,7 @@ package registry
 import (
 	"context"
 	"errors"
+	"fmt"
 	"strings"
 	"sync"
 	"time"
@@ -158,7 +159,7 @@ func (s *SmartContractRegistry) refreshLoop() {
 func (s *SmartContractRegistry) refreshData() error {
 	fromContract, err := s.loadUnfilteredFromContract()
 	if err != nil {
-		return err
+		return fmt.Errorf("could not load nodes from contract: %v", err)
 	}
 
 	newNodes := []Node{}

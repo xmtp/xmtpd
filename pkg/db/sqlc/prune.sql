e60770130548990adfb583a95c46bb3bdcb64bd2
Martin Kysel <code@martinkysel.com>
2025-05-08 19:55:23 +0100

    Initial prune implementation

---
+++ pkg/db/sqlc/prune.sql
@@ -0,0 +1,15 @@
+-- name: DeleteExpiredEnvelopesBatch :many
+WITH to_delete AS (
+    SELECT originator_node_id, originator_sequence_id
+    FROM gateway_envelopes
+    WHERE expiry IS NOT NULL
+      AND expiry < EXTRACT(EPOCH FROM now())::bigint
+    ORDER BY expiry
+    LIMIT 1000
+        FOR UPDATE SKIP LOCKED
+)
+DELETE FROM gateway_envelopes ge
+    USING to_delete td
+WHERE ge.originator_node_id = td.originator_node_id
+  AND ge.originator_sequence_id = td.originator_sequence_id
+RETURNING ge.originator_node_id, ge.originator_sequence_id, ge.expiry;
--- pkg/prune/prune.go
+++ pkg/prune/prune.go
@@ -3,6 +3,9 @@ package prune
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"github.com/xmtp/xmtpd/pkg/db/queries"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -26,5 +29,34 @@ func NewPruneExecutor(
 }
 
 func (e *Executor) Run() error {
+	querier := queries.New(e.writerDB)
+	totalDeletionCount := 0
+	start := time.Now()
+
+	for {
+		rows, err := querier.DeleteExpiredEnvelopesBatch(e.ctx)
+		if err != nil {
+			return err
+		}
+
+		if len(rows) == 0 {
+			break
+		}
+
+		totalDeletionCount = totalDeletionCount + len(rows)
+
+		e.log.Info("Pruned expired envelopes batch", zap.Int("count", len(rows)))
+
+		for row := range rows {
+			e.log.Debug(fmt.Sprintf("Pruning expired envelopes batch row: %d", row))
+		}
+	}
+
+	if totalDeletionCount == 0 {
+		e.log.Info("No expired envelopes found")
+	}
+
+	e.log.Info("Done", zap.Int("pruned count", totalDeletionCount), zap.Duration("elapsed", time.Since(start)))
+
 	return nil
 }

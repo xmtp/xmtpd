5f28cacf27328922e6921b27de9e0bcd92d6c50d
Martin Kysel <code@martinkysel.com>
2025-05-12 14:39:03 -0400

    Initial prune implementation (#774)
    I expect to make a few changes as we progress such as dry-run and a
    limit to the number of cycles it takes.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced configurable batch pruning of expired envelopes with
    options for maximum cycles and dry run mode.
    - **Enhancements**
    - Added detailed logging of expired envelope counts and pruning progress
    during execution.
    - **Chores**
    - Improved internal expired data management with efficient batch
    deletion and concurrency-safe locking.
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- pkg/prune/prune.go
+++ pkg/prune/prune.go
@@ -3,6 +3,12 @@ package prune
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/xmtp/xmtpd/pkg/config"
+
+	"github.com/xmtp/xmtpd/pkg/db/queries"
 
 	"go.uber.org/zap"
 )
@@ -11,20 +17,75 @@ type Executor struct {
 	ctx      context.Context
 	log      *zap.Logger
 	writerDB *sql.DB
+	config   *config.PruneConfig
 }
 
 func NewPruneExecutor(
 	ctx context.Context,
 	log *zap.Logger,
 	writerDB *sql.DB,
+	config *config.PruneConfig,
 ) *Executor {
 	return &Executor{
 		ctx:      ctx,
 		log:      log,
 		writerDB: writerDB,
+		config:   config,
 	}
 }
 
 func (e *Executor) Run() error {
+	querier := queries.New(e.writerDB)
+	totalDeletionCount := 0
+	start := time.Now()
+
+	cnt, err := querier.CountExpiredEnvelopes(e.ctx)
+	if err != nil {
+		return err
+	}
+	e.log.Info("Count of envelopes eligible for pruning", zap.Int64("count", cnt))
+
+	if e.config.DryRun {
+		e.log.Info("Dry run mode enabled. Nothing to do")
+		return nil
+	}
+
+	cyclesCompleted := 0
+
+	for {
+		if cyclesCompleted >= e.config.MaxCycles {
+			e.log.Warn("Reached maximum pruning cycles", zap.Int("maxCycles", e.config.MaxCycles))
+			break
+		}
+
+		rows, err := querier.DeleteExpiredEnvelopesBatch(e.ctx)
+		if err != nil {
+			return err
+		}
+
+		if len(rows) == 0 {
+			break
+		}
+
+		totalDeletionCount = totalDeletionCount + len(rows)
+
+		e.log.Info("Pruned expired envelopes batch", zap.Int("count", len(rows)))
+
+		for _, row := range rows {
+			e.log.Debug(fmt.Sprintf("Pruning expired envelopes batch row: %v", row))
+		}
+		cyclesCompleted++
+	}
+
+	if totalDeletionCount == 0 {
+		e.log.Info("No expired envelopes found")
+	}
+
+	e.log.Info(
+		"Done",
+		zap.Int("pruned count", totalDeletionCount),
+		zap.Duration("elapsed", time.Since(start)),
+	)
+
 	return nil
 }

88be4c700becd115544c2599320b16bda761c82e
Borja Aranda <borja@ephemerahq.com>
2025-05-15 21:55:24 +0200

    Move appchain contracts to their own folder

---
+++ pkg/indexer/block_tracker/blockTracker.go
@@ -0,0 +1,137 @@
+package blocktracker
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/xmtp/xmtpd/pkg/db/queries"
+)
+
+/*
+*
+BlockTracker keeps a database record of the latest block that has been indexed for a contract address
+and allows the user to increase the value.
+*
+*/
+type BlockTracker struct {
+	latestBlock     *Block
+	contractAddress string
+	queries         *queries.Queries
+	mu              sync.Mutex
+}
+
+type Block struct {
+	number atomic.Uint64
+	hash   common.Hash
+}
+
+var ErrEmptyBlockHash = errors.New("block hash is empty")
+
+// Return a new BlockTracker initialized to the latest block from the DB
+func NewBlockTracker(
+	ctx context.Context,
+	contractAddress string,
+	queries *queries.Queries,
+) (*BlockTracker, error) {
+	bt := &BlockTracker{
+		contractAddress: contractAddress,
+		queries:         queries,
+	}
+
+	latestBlock, err := loadLatestBlock(ctx, contractAddress, queries)
+	if err != nil {
+		return nil, err
+	}
+	bt.latestBlock = latestBlock
+
+	return bt, nil
+}
+
+func (bt *BlockTracker) GetLatestBlock() (uint64, []byte) {
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+	return bt.latestBlock.number.Load(), bt.latestBlock.hash.Bytes()
+}
+
+func (bt *BlockTracker) UpdateLatestBlock(
+	ctx context.Context,
+	block uint64,
+	hashBytes []byte,
+) error {
+	// Quick check without lock
+	if block <= bt.latestBlock.number.Load() {
+		return nil
+	}
+
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+
+	// Re-check after acquiring lock
+	if block <= bt.latestBlock.number.Load() {
+		return nil
+	}
+
+	newHash := common.Hash(hashBytes)
+
+	if newHash == (common.Hash{}) {
+		return ErrEmptyBlockHash
+	}
+
+	if newHash == bt.latestBlock.hash {
+		return nil
+	}
+
+	if err := bt.updateDB(ctx, block, newHash.Bytes()); err != nil {
+		return err
+	}
+
+	bt.latestBlock.number.Store(block)
+	bt.latestBlock.hash = newHash
+
+	return nil
+}
+
+func (bt *BlockTracker) updateDB(ctx context.Context, block uint64, hash []byte) error {
+	return bt.queries.SetLatestBlock(ctx, queries.SetLatestBlockParams{
+		ContractAddress: bt.contractAddress,
+		BlockNumber:     int64(block),
+		BlockHash:       hash,
+	})
+}
+
+func loadLatestBlock(
+	ctx context.Context,
+	contractAddress string,
+	querier *queries.Queries,
+) (*Block, error) {
+	block := &Block{
+		number: atomic.Uint64{},
+		hash:   common.Hash{},
+	}
+
+	latestBlock, err := querier.GetLatestBlock(ctx, contractAddress)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return block, nil
+		}
+		return block, err
+	}
+
+	if latestBlock.BlockNumber < 0 {
+		return block, fmt.Errorf(
+			"invalid block number %d for contract %s",
+			latestBlock.BlockNumber,
+			contractAddress,
+		)
+	}
+
+	block.number.Store(uint64(latestBlock.BlockNumber))
+	block.hash = common.BytesToHash(latestBlock.BlockHash)
+
+	return block, nil
+}
---
+++ pkg/indexer/block_tracker/blockTracker_test.go
@@ -0,0 +1,165 @@
+package blocktracker_test
+
+import (
+	"context"
+	"sync"
+	"testing"
+
+	"github.com/xmtp/xmtpd/pkg/indexer/blocktracker"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+	"github.com/xmtp/xmtpd/pkg/db/queries"
+	"github.com/xmtp/xmtpd/pkg/testutils"
+)
+
+const CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+func TestInitialize(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	db, _ := testutils.NewDB(t, ctx)
+	querier := queries.New(db)
+
+	tracker, err := blocktracker.NewBlockTracker(ctx, CONTRACT_ADDRESS, querier)
+	require.NoError(t, err)
+	blockNumber, blockHash := tracker.GetLatestBlock()
+	require.NoError(t, err)
+	require.NotNil(t, tracker)
+	require.Equal(t, uint64(0), blockNumber)
+	require.Equal(t, common.Hash{}.Bytes(), blockHash)
+}
+
+func TestUpdateLatestBlock(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	db, _ := testutils.NewDB(t, ctx)
+	querier := queries.New(db)
+
+	tracker, err := blocktracker.NewBlockTracker(ctx, CONTRACT_ADDRESS, querier)
+	require.NoError(t, err)
+
+	blockHigh := testutils.Int64ToHash(100).Bytes()
+	blockLow := testutils.Int64ToHash(50).Bytes()
+
+	// Test updating to a higher block
+	err = tracker.UpdateLatestBlock(ctx, 100, blockHigh)
+	blockNumber, blockHash := tracker.GetLatestBlock()
+	require.NoError(t, err)
+	require.Equal(t, uint64(100), blockNumber)
+	require.Equal(t, blockHigh, blockHash)
+
+	// Test updating to a lower block (should not update)
+	err = tracker.UpdateLatestBlock(ctx, 50, blockLow)
+	require.NoError(t, err)
+	blockNumber, blockHash = tracker.GetLatestBlock()
+	require.Equal(t, uint64(100), blockNumber)
+	require.Equal(t, blockHigh, blockHash)
+
+	// Test updating to the same block (should not update)
+	err = tracker.UpdateLatestBlock(ctx, 100, blockHigh)
+	require.NoError(t, err)
+	blockNumber, blockHash = tracker.GetLatestBlock()
+	require.Equal(t, uint64(100), blockNumber)
+	require.Equal(t, blockHigh, blockHash)
+
+	// Verify persistence
+	newTracker, err := blocktracker.NewBlockTracker(ctx, CONTRACT_ADDRESS, querier)
+	require.NoError(t, err)
+	blockNumber, blockHash = newTracker.GetLatestBlock()
+	require.Equal(t, uint64(100), blockNumber)
+	require.Equal(t, blockHigh, blockHash)
+}
+
+func TestConcurrentUpdates(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	db, _ := testutils.NewDB(t, ctx)
+	querier := queries.New(db)
+
+	tracker, err := blocktracker.NewBlockTracker(ctx, CONTRACT_ADDRESS, querier)
+	require.NoError(t, err)
+
+	var wg sync.WaitGroup
+	numGoroutines := 10
+	updatesPerGoroutine := 100
+
+	// Launch multiple goroutines to update the block number
+	for i := 0; i < numGoroutines; i++ {
+		wg.Add(1)
+		go func(startBlock int) {
+			defer wg.Done()
+			for j := 0; j < updatesPerGoroutine; j++ {
+				blockNum := uint64(startBlock + j)
+				err := tracker.UpdateLatestBlock(
+					ctx,
+					blockNum,
+					testutils.Int64ToHash(int64(blockNum)).Bytes(),
+				)
+				require.NoError(t, err)
+			}
+		}(i * updatesPerGoroutine)
+	}
+
+	wg.Wait()
+
+	// The final block number should be the highest one attempted
+	expectedFinalBlock := uint64((numGoroutines-1)*updatesPerGoroutine + (updatesPerGoroutine - 1))
+	blockNumber, blockHash := tracker.GetLatestBlock()
+	require.Equal(t, expectedFinalBlock, blockNumber)
+
+	expectedFinalHash := testutils.Int64ToHash(int64(expectedFinalBlock)).Bytes()
+	require.Equal(t, expectedFinalHash, blockHash)
+
+	// Verify persistence
+	newTracker, err := blocktracker.NewBlockTracker(ctx, CONTRACT_ADDRESS, querier)
+	require.NoError(t, err)
+	blockNumber, blockHash = newTracker.GetLatestBlock()
+	require.Equal(t, expectedFinalBlock, blockNumber)
+	require.Equal(t, expectedFinalHash, blockHash)
+}
+
+func TestMultipleContractAddresses(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	db, _ := testutils.NewDB(t, ctx)
+	querier := queries.New(db)
+
+	address1 := "0x0000000000000000000000000000000000000001"
+	address2 := "0x0000000000000000000000000000000000000002"
+
+	tracker1, err := blocktracker.NewBlockTracker(ctx, address1, querier)
+	require.NoError(t, err)
+	tracker2, err := blocktracker.NewBlockTracker(ctx, address2, querier)
+	require.NoError(t, err)
+
+	blockHash1 := testutils.Int64ToHash(100).Bytes()
+	blockHash2 := testutils.Int64ToHash(200).Bytes()
+
+	// Update trackers independently
+	err = tracker1.UpdateLatestBlock(ctx, 100, blockHash1)
+	require.NoError(t, err)
+	err = tracker2.UpdateLatestBlock(ctx, 200, blockHash2)
+	require.NoError(t, err)
+
+	// Verify different addresses maintain separate block numbers
+	blockNumber, blockHash := tracker1.GetLatestBlock()
+	require.Equal(t, uint64(100), blockNumber)
+	require.Equal(t, blockHash1, blockHash)
+	blockNumber, blockHash = tracker2.GetLatestBlock()
+	require.Equal(t, uint64(200), blockNumber)
+	require.Equal(t, blockHash2, blockHash)
+
+	// Verify persistence for both addresses
+	newTracker1, err := blocktracker.NewBlockTracker(ctx, address1, querier)
+	require.NoError(t, err)
+	newTracker2, err := blocktracker.NewBlockTracker(ctx, address2, querier)
+	require.NoError(t, err)
+
+	blockNumber, blockHash = newTracker1.GetLatestBlock()
+	require.Equal(t, uint64(100), blockNumber)
+	require.Equal(t, blockHash1, blockHash)
+	blockNumber, blockHash = newTracker2.GetLatestBlock()
+	require.Equal(t, uint64(200), blockNumber)
+	require.Equal(t, blockHash2, blockHash)
+}

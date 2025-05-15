3fbf0cf94218ade194ddad4515166329f77a0ed7
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2025-05-15 11:29:02 -0400

    Add submission worker

--- pkg/payerreport/workers/timers.go
+++ pkg/payerreport/workers/timers.go
@@ -5,11 +5,11 @@ import "time"
 // Distributes the minute of the hour that each node will run operations on.
 // This is to avoid having many nodes do identical work at the same time, and potentially leading to
 // duplicate reports or submissions.
-func findNextRunTime(myNodeID uint32) time.Time {
+func findNextRunTime(myNodeID uint32, workerID uint32) time.Time {
 	// Use a hash function to distribute node IDs evenly across minutes
 	// regardless of their numerical distribution
 	// Uses a Knuth multiplicative hash function
-	minuteOffset := int(((myNodeID * 2654435761) >> 16) % 60)
+	minuteOffset := int((((myNodeID + workerID) * 2654435761) >> 16) % 60)
 
 	now := time.Now().UTC()
 	// Next run is at the „minuteOffset" of the current hour unless that

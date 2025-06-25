ddd6b92412db284239cc9a758a9a45440d000454
Martin Kysel <code@martinkysel.com>
2025-06-24 21:52:00 -0700

    Skip attestation test until a fix is found

--- pkg/payerreport/workers/integration_test.go
+++ pkg/payerreport/workers/integration_test.go
@@ -396,6 +396,7 @@ func TestCanGenerateReport(t *testing.T) {
 }
 
 func TestCanGenerateAndAttestReport(t *testing.T) {
+	t.Skip("This test is flaky. See https://github.com/xmtp/xmtpd/issues/913")
 	scaffold := setupMultiNodeTest(t)
 	groupID := testutils.RandomGroupID()
 	messageTopic := topic.NewTopic(topic.TOPIC_KIND_GROUP_MESSAGES_V1, groupID[:]).Bytes()

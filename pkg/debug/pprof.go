792b453960d1b939bda2934fae4726bce6677e9d
Francisco de Borja Aranda Castillejo <borja@ephemerahq.com>
2025-07-11 08:28:07 +0200

    Add pprof http server (#923)
    ### Add pprof HTTP server to replication command with configurable debug
    options
    - Adds `DebugOptions` struct to
    [pkg/config/options.go](https://github.com/xmtp/xmtpd/pull/923/files#diff-6731fb6f709392ce3e37d3b0c42074cddbce566dad2bab86af24ba7585eeb57c)
    with `Enable` and `Port` fields for configuring the debug server
    - Creates new debug package in
    [pkg/debug/pprof.go](https://github.com/xmtp/xmtpd/pull/923/files#diff-1968d013ec8ca592e3913c347601c09f23ec3096dc66242e1c3ecb33bd512a70)
    with `Server` struct that provides `Start` and `Shutdown` methods for
    managing pprof HTTP endpoints
    - Modifies
    [cmd/replication/main.go](https://github.com/xmtp/xmtpd/pull/923/files#diff-0d69b50851c3a886b12eba860f887169c16cf724884bd031a7be4a7f556aee85)
    to conditionally start the pprof server when debug options are enabled
    
    #### 📍Where to Start
    Start with the main function in
    [cmd/replication/main.go](https://github.com/xmtp/xmtpd/pull/923/files#diff-0d69b50851c3a886b12eba860f887169c16cf724884bd031a7be4a7f556aee85)
    where the pprof server initialization logic is added.
    
    
    
    #### Changes since #923 opened
    
    - Added deferred shutdown function for pprof debug server [f8ec6a1]
    ----
    
    _[Macroscope](https://app.macroscope.com) summarized f8ec6a1._

---
+++ pkg/debug/pprof.go
@@ -0,0 +1,55 @@
+package debug
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"time"
+)
+
+type Server struct {
+	server *http.Server
+	port   int
+}
+
+func NewServer(port int) *Server {
+	return &Server{
+		port: port,
+	}
+}
+
+func (s *Server) Start(ctx context.Context) error {
+	mux := http.NewServeMux()
+
+	// Register pprof endpoints - they are automatically registered to the default ServeMux.
+	mux.Handle("/debug/pprof/", http.DefaultServeMux)
+
+	s.server = &http.Server{
+		Addr:         fmt.Sprintf(":%d", s.port),
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  60 * time.Second,
+	}
+
+	errChan := make(chan error, 1)
+	go func() {
+		errChan <- s.server.ListenAndServe()
+	}()
+
+	select {
+	case <-ctx.Done():
+		return s.Shutdown(context.Background())
+	case err := <-errChan:
+		return err
+	}
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	if s.server == nil {
+		return nil
+	}
+
+	return s.server.Shutdown(ctx)
+}

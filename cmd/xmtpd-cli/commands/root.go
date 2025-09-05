50c0290a3df866562b2b7fb5fb45ee1da02b75a6
Martin Kysel <code@martinkysel.com>
2025-09-05 15:44:42 -0400

    autocomp

--- cmd/xmtpd-cli/commands/root.go
+++ cmd/xmtpd-cli/commands/root.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -125,4 +126,26 @@ func cliLogger() (*zap.Logger, error) {
 	}
 
 	return l, nil
+}
+
+func init() {
+	// Add a hidden command to generate completion scripts
+	rootCmd.AddCommand(&cobra.Command{
+		Use:       "completion [bash|zsh|fish|powershell]",
+		Short:     "Generate shell completion script",
+		ValidArgs: []string{"bash", "zsh", "fish", "powershell"},
+		Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+		Run: func(cmd *cobra.Command, args []string) {
+			switch args[0] {
+			case "bash":
+				_ = rootCmd.GenBashCompletion(os.Stdout)
+			case "zsh":
+				_ = rootCmd.GenZshCompletion(os.Stdout)
+			case "fish":
+				_ = rootCmd.GenFishCompletion(os.Stdout, true)
+			case "powershell":
+				_ = rootCmd.GenPowerShellCompletionWithDesc(os.Stdout)
+			}
+		},
+	})
 }

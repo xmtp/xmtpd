3820974da10cf0eb8a912ef965edc95b2b06a2eb
Francisco de Borja Aranda Castillejo <borja@ephemerahq.com>
2025-09-29 20:28:44 +0200

    Allow gateway to enable gRPC reflection (#1185)
    ### Add gateway configuration and builder support to enable gRPC server
    reflection for the Gateway service
    - Add `Reflection` field to `config.GatewayConfig` with struct tags
    `group:"Reflection Options"` and `namespace:"reflection"` in
    [gateway.go](https://github.com/xmtp/xmtpd/pull/1185/files#diff-79af78983749015479ea26d687893200bbfad7448eef8f0b97b9f178d3a18c8c)
    - Add `gateway.GatewayServiceBuilder.WithReflection` to set
    `b.config.Reflection.Enable` in
    [builder.go](https://github.com/xmtp/xmtpd/pull/1185/files#diff-27a239accb0576c3da95c1fea41bd81227bc4355f38be11a66d7b15135ea8e61)
    - Update `gateway.GatewayServiceBuilder.buildGatewayService` to
    conditionally register gRPC reflection via
    `reflection.Register(grpcServer)` and log when enabled in
    [builder.go](https://github.com/xmtp/xmtpd/pull/1185/files#diff-27a239accb0576c3da95c1fea41bd81227bc4355f38be11a66d7b15135ea8e61)
    
    #### 📍Where to Start
    Start with the service registration path in
    `gateway.GatewayServiceBuilder.buildGatewayService` in
    [builder.go](https://github.com/xmtp/xmtpd/pull/1185/files#diff-27a239accb0576c3da95c1fea41bd81227bc4355f38be11a66d7b15135ea8e61).
    
    ----
    
    _[Macroscope](https://app.macroscope.com) summarized 27d35bc._

--- pkg/config/gateway.go
+++ pkg/config/gateway.go
@@ -1,11 +1,12 @@
 package config
 
 type GatewayConfig struct {
-	API       APIOptions       `group:"API Options"            namespace:"api"`
-	Contracts ContractsOptions `group:"Contracts Options"      namespace:"contracts"`
-	Log       LogOptions       `group:"Log Options"            namespace:"log"`
-	Metrics   MetricsOptions   `group:"Metrics Options"        namespace:"metrics"`
-	Payer     PayerOptions     `group:"Payer Options"          namespace:"payer"`
-	Redis     RedisOptions     `group:"Redis Options"          namespace:"redis"`
-	Tracing   TracingOptions   `group:"DD APM Tracing Options" namespace:"tracing"`
+	API        APIOptions        `group:"API Options"            namespace:"api"`
+	Contracts  ContractsOptions  `group:"Contracts Options"      namespace:"contracts"`
+	Log        LogOptions        `group:"Log Options"            namespace:"log"`
+	Metrics    MetricsOptions    `group:"Metrics Options"        namespace:"metrics"`
+	Payer      PayerOptions      `group:"Payer Options"          namespace:"payer"`
+	Redis      RedisOptions      `group:"Redis Options"          namespace:"redis"`
+	Tracing    TracingOptions    `group:"DD APM Tracing Options" namespace:"tracing"`
+	Reflection ReflectionOptions `group:"Reflection Options"     namespace:"reflection"`
 }

613735c67a77ce08b60bf7e3ea378004b2b20627
Maelkum <aleksandar.cekrlic@gmail.com>
2025-12-30 14:50:36 +0100

    Remove unnecessary return value

--- pkg/api/message/envelope_list.go
+++ pkg/api/message/envelope_list.go
@@ -139,17 +139,15 @@ func (s *subscriptionHandler) allSubscriptions() <-chan []queries.SelectGatewayE
 	return s.mergedSubs.output()
 }
 
-func (s *subscriptionHandler) removeSubscription(id uint32) error {
+func (s *subscriptionHandler) removeSubscription(id uint32) {
 	s.Lock()
 	defer s.Unlock()
 
 	poller, ok := s.subs[id]
 	if !ok {
-		return nil
+		return
 	}
 
 	poller.cancel()
 	delete(s.subs, id)
-
-	return nil
 }

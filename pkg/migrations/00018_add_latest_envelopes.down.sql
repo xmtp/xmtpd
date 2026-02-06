0be8e6cc68f2a70da2c7bd94d22ca6c95362c555
Maelkum <aleksandar.cekrlic@gmail.com>
2026-02-06 13:02:14 +0100

    Add reinstating migrations to previous state

--- pkg/migrations/00018_add_latest_envelopes.down.sql
+++ pkg/migrations/00018_add_latest_envelopes.down.sql
@@ -1,2 +1,9 @@
+-- First reinstate previous version of the trigger, as defined in 00006_add_latest_envelopes.up.sql
+CREATE TRIGGER gateway_latest_upd
+    AFTER INSERT ON gateway_envelopes_meta
+    FOR EACH ROW EXECUTE FUNCTION update_latest_envelope();
+
+
 DROP TRIGGER IF EXISTS gateway_latest_upd_v2 ON gateway_envelopes_meta;
 DROP FUNCTION IF EXISTS update_latest_envelope_v2();
+

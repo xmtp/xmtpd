7684c6bf2943420f337890df6a0b04e102d70a1d
Martin Kysel <code@martinkysel.com>
2026-02-17 16:14:55 -0500

    Remove duplicate log

--- pkg/api/metadata/service.go
+++ pkg/api/metadata/service.go
@@ -50,7 +50,6 @@ func (s *Service) GetSyncCursor(
 	_ context.Context,
 	req *connect.Request[metadata_api.GetSyncCursorRequest],
 ) (*connect.Response[metadata_api.GetSyncCursorResponse], error) {
-	s.logger.Info("received request", utils.MethodField(req.Spec().Procedure))
 	if s.logger.Core().Enabled(zap.DebugLevel) {
 		s.logger.Debug("received request", utils.MethodField(req.Spec().Procedure))
 	}

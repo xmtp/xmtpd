934b0558110f20f79e13919f028aa371313640b6
Aleksandar Čekrlić <aleksandar.cekrlic@gmail.com>
2026-02-19 11:43:28 +0100

    Introduce database worker to check and create partitions (#1608)
    This PR introduces a background database worker that will periodically
    check existing partitions and create new ones if the existing partitions
    are sufficiently full. In the future this database worker can be
    extended to run more background tasks, as needed.

---
+++ pkg/db/worker/worker.go
@@ -0,0 +1,191 @@
+package worker
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math"
+	"sync"
+	"time"
+
+	"go.uber.org/zap"
+
+	"github.com/xmtp/xmtpd/pkg/db"
+	"github.com/xmtp/xmtpd/pkg/db/queries"
+	"github.com/xmtp/xmtpd/pkg/utils"
+)
+
+const (
+	DefaultFillThreshold = 0.7
+	DefaultCheckInterval = 30 * time.Minute
+)
+
+var defaultConfig = Config{
+	Interval: DefaultCheckInterval,
+	Partition: PartitionConfig{
+		FillThreshold: DefaultFillThreshold,
+	},
+}
+
+type Config struct {
+	Interval  time.Duration
+	Partition PartitionConfig
+}
+
+// partition config controls when should the database worker create new partitions.
+// TODO: Perhaps use a lis of originators and setup partitions even for nodes that do not have any yet.
+
+type PartitionConfig struct {
+	FillThreshold float64
+}
+
+type Worker struct {
+	cfg Config
+
+	lock *sync.RWMutex
+
+	log *zap.Logger
+	db  *db.Handler
+
+	createdPartitions map[partitionHeader]struct{}
+}
+
+func NewWorker(log *zap.Logger, db *db.Handler) *Worker {
+	return newWorkerWithConfig(defaultConfig, log, db)
+}
+
+func newWorkerWithConfig(cfg Config, log *zap.Logger, db *db.Handler) *Worker {
+	worker := &Worker{
+		cfg:               cfg,
+		log:               log.Named(utils.DatabaseWorkerLoggerName),
+		db:                db,
+		lock:              &sync.RWMutex{},
+		createdPartitions: make(map[partitionHeader]struct{}),
+	}
+
+	return worker
+}
+
+type partitionHeader struct {
+	nodeID     uint32
+	startSeqID uint64
+}
+
+func (w *Worker) Start(ctx context.Context) error {
+	err := w.runDBCheck(ctx)
+	if err != nil {
+		w.log.Error("database check failed", zap.Error(err))
+		// Not stopping on this error.
+	}
+
+	go func() {
+		ticker := time.NewTicker(w.cfg.Interval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				w.log.Info("context done, stopping")
+				return
+
+			case <-ticker.C:
+				w.log.Debug("running db check")
+				err := w.runDBCheck(ctx)
+				if err != nil {
+					w.log.Error("database check failed", zap.Error(err))
+				}
+
+				// On error - do nothing.
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (w *Worker) runDBCheck(ctx context.Context) error {
+	le, err := w.db.ReadQuery().SelectVectorClock(ctx)
+	if err != nil {
+		return fmt.Errorf("could not retrieve vector clock: %w", err)
+	}
+
+	vc := db.ToVectorClock(le)
+
+	var errs []error
+
+	for nodeID, seqID := range vc {
+
+		// NOTE: We're doing uint64 => int64 conversion and arithmethic, so let's be pedantic.
+		// Not sure how soon we can expect to see this.
+		if seqID > math.MaxInt64 {
+			w.log.Warn("sequence ID value larger than int64 range", zap.Uint64("value", seqID))
+			continue
+		}
+
+		err = w.runPartitionCheck(ctx, nodeID, int64(seqID))
+		if err != nil {
+			errs = append(
+				errs,
+				fmt.Errorf("partition check failed for node (id: %v): %w", nodeID, err),
+			)
+		}
+	}
+
+	return errors.Join(errs...)
+}
+
+func (w *Worker) runPartitionCheck(ctx context.Context, nodeID uint32, seqID int64) error {
+	partitionSize := db.GatewayEnvelopeBandWidth
+
+	fillRatio := float64(seqID%partitionSize) / float64(partitionSize)
+
+	w.log.Debug("partition fill ratio",
+		utils.OriginatorIDField(nodeID),
+		utils.SequenceIDField(seqID),
+		zap.String("filled_percentage", fmt.Sprintf("%.2f", fillRatio)))
+
+	// Partition has enough room left, continue.
+	if fillRatio <= w.cfg.Partition.FillThreshold {
+		return nil
+	}
+
+	targetSeqID := seqID + partitionSize
+	if targetSeqID < 0 {
+		return fmt.Errorf("sequence ID overflow (value: %v)", seqID)
+	}
+
+	startSeqID := uint64((targetSeqID / partitionSize) * partitionSize)
+
+	w.lock.RLock()
+	_, exists := w.createdPartitions[partitionHeader{nodeID: nodeID, startSeqID: startSeqID}]
+	w.lock.RUnlock()
+
+	if exists {
+		w.log.Info("partition already created, skipping",
+			utils.OriginatorIDField(nodeID),
+			zap.Uint64("start_sequence_id", startSeqID),
+		)
+		return nil
+	}
+
+	params := queries.EnsureGatewayPartsParams{
+		OriginatorNodeID:     int32(nodeID),
+		OriginatorSequenceID: targetSeqID,
+		BandWidth:            partitionSize,
+	}
+	err := w.db.WriteQuery().EnsureGatewayParts(ctx, params)
+	if err != nil {
+		return fmt.Errorf("could not create gateway partitions: %w", err)
+	}
+
+	w.log.Info("created partition for node",
+		utils.OriginatorIDField(nodeID),
+		zap.Uint64("start_sequence_id", startSeqID),
+	)
+
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	w.createdPartitions[partitionHeader{nodeID: nodeID, startSeqID: startSeqID}] = struct{}{}
+
+	return nil
+}
---
+++ pkg/db/worker/worker_internal_test.go
@@ -0,0 +1,217 @@
+package worker
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/xmtp/xmtpd/pkg/db"
+	"github.com/xmtp/xmtpd/pkg/db/queries"
+	"github.com/xmtp/xmtpd/pkg/testutils"
+	envelopeTestUtils "github.com/xmtp/xmtpd/pkg/testutils/envelopes"
+	"github.com/xmtp/xmtpd/pkg/topic"
+)
+
+func TestWorker_CheckPartition(t *testing.T) {
+	t.Run("empty partition", func(t *testing.T) {
+		ts := newTestScaffold(t)
+
+		err := ts.worker.runPartitionCheck(t.Context(), 100, 0)
+		require.NoError(t, err)
+		require.Empty(t, readPartitionSet(t, ts.db))
+	})
+	t.Run("partitions with enough room", func(t *testing.T) {
+		ts := newTestScaffold(t)
+
+		envelopes := map[uint32]uint64{
+			100: 1,
+			200: 100_000,
+			300: 500_000,
+		}
+
+		testutils.InsertGatewayEnvelopes(t, ts.db.DB(), generateEnvelopes(t, envelopes))
+		require.Len(t, readPartitionSet(t, ts.db), len(envelopes))
+
+		err := ts.worker.runDBCheck(t.Context())
+		require.NoError(t, err)
+
+		// No new partitions should be created.
+		require.Len(t, readPartitionSet(t, ts.db), len(envelopes))
+	})
+	t.Run("checks create new partitions", func(t *testing.T) {
+		ts := newTestScaffold(t)
+
+		require.Empty(t, readPartitionSet(t, ts.db))
+
+		envelopes := map[uint32]uint64{
+			100: 1,
+			200: 900_000,
+			300: 750_000,
+		}
+
+		testutils.InsertGatewayEnvelopes(t, ts.db.DB(), generateEnvelopes(t, envelopes))
+		require.Len(t, readPartitionSet(t, ts.db), len(envelopes))
+
+		err := ts.worker.runDBCheck(t.Context())
+		require.NoError(t, err)
+
+		// Two new partitions should be created.
+		partitions := readPartitionSet(t, ts.db)
+		require.Len(t, partitions, 5)
+
+		_, ok := partitions["gateway_envelopes_meta_o100_s1000000_2000000"]
+		require.False(t, ok)
+
+		_, ok = partitions["gateway_envelopes_meta_o200_s1000000_2000000"]
+		require.True(t, ok)
+
+		_, ok = partitions["gateway_envelopes_meta_o300_s1000000_2000000"]
+		require.True(t, ok)
+	})
+	t.Run("checks create new higher partitions", func(t *testing.T) {
+		ts := newTestScaffold(t)
+
+		envelopes := map[uint32]uint64{
+			100: 50,
+			200: 3_750_000,
+		}
+
+		testutils.InsertGatewayEnvelopes(t, ts.db.DB(), generateEnvelopes(t, envelopes))
+		require.Len(t, readPartitionSet(t, ts.db), len(envelopes))
+
+		err := ts.worker.runDBCheck(t.Context())
+		require.NoError(t, err)
+
+		// One new partition should be created.
+		partitions := readPartitionSet(t, ts.db)
+		require.Len(t, partitions, 3)
+
+		_, ok := partitions["gateway_envelopes_meta_o100_s1000000_2000000"]
+		require.False(t, ok)
+
+		_, ok = partitions["gateway_envelopes_meta_o200_s3000000_4000000"]
+		require.True(t, ok)
+	})
+	t.Run("trying to create the same partition multiple times does nothing", func(t *testing.T) {
+		ts := newTestScaffold(t)
+
+		envelopes := map[uint32]uint64{
+			100: 4_900_000,
+		}
+
+		testutils.InsertGatewayEnvelopes(t, ts.db.DB(), generateEnvelopes(t, envelopes))
+		require.Len(t, readPartitionSet(t, ts.db), 1)
+
+		err := ts.worker.runDBCheck(t.Context())
+		require.NoError(t, err)
+
+		// One new partition should be created.
+		partitions := readPartitionSet(t, ts.db)
+		require.Len(t, partitions, 2)
+
+		_, ok := partitions["gateway_envelopes_meta_o100_s4000000_5000000"]
+		require.True(t, ok)
+
+		_, ok = partitions["gateway_envelopes_meta_o100_s5000000_6000000"]
+		require.True(t, ok)
+
+		err = ts.worker.runDBCheck(t.Context())
+		require.NoError(t, err)
+
+		// No new partitions should be created.
+		partitions = readPartitionSet(t, ts.db)
+		require.Len(t, partitions, 2)
+	})
+}
+
+func readPartitionSet(t *testing.T, db *db.Handler) map[string]struct{} {
+	t.Helper()
+
+	query := `SELECT table_name
+			  	FROM information_schema.tables
+				WHERE table_name ~ '^gateway_envelopes_meta_o\d+_s\d+_\d+$'`
+
+	rows, err := db.DB().QueryContext(t.Context(), query)
+	require.NoError(t, err)
+
+	defer func() {
+		err = rows.Close()
+		require.NoError(t, err)
+
+		err = rows.Err()
+		require.NoError(t, err)
+	}()
+
+	out := make(map[string]struct{})
+	for rows.Next() {
+
+		var tableName string
+		err := rows.Scan(&tableName)
+		require.NoError(t, err)
+
+		out[tableName] = struct{}{}
+	}
+
+	return out
+}
+
+func generateEnvelopes(
+	t *testing.T,
+	seqIDs map[uint32]uint64,
+) []queries.InsertGatewayEnvelopeParams {
+	t.Helper()
+
+	topic := topic.NewTopic(
+		topic.TopicKindGroupMessagesV1,
+		[]byte(fmt.Sprintf("generic-topic-%v", rand.Int())),
+	)
+
+	out := make([]queries.InsertGatewayEnvelopeParams, 0, len(seqIDs))
+
+	for nodeID, seqID := range seqIDs {
+
+		oe := testutils.Marshal(
+			t,
+			envelopeTestUtils.CreateOriginatorEnvelopeWithTopic(
+				t,
+				uint32(nodeID),
+				uint64(seqID),
+				topic.Bytes(),
+			),
+		)
+
+		out = append(out, queries.InsertGatewayEnvelopeParams{
+			OriginatorNodeID:     int32(nodeID),
+			OriginatorSequenceID: int64(seqID),
+			Topic:                topic.Bytes(),
+			OriginatorEnvelope:   oe,
+		})
+
+	}
+
+	return out
+}
+
+type testScaffold struct {
+	db     *db.Handler
+	worker *Worker
+}
+
+func newTestScaffold(t *testing.T) testScaffold {
+	t.Helper()
+
+	var (
+		db, _ = testutils.NewDB(t, t.Context())
+		log   = testutils.NewLog(t)
+	)
+
+	worker := NewWorker(log, db)
+
+	ts := testScaffold{
+		db:     db,
+		worker: worker,
+	}
+
+	return ts
+}

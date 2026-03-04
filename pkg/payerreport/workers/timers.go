07735b4281c9528be2c626fff82164de16e24731
eroderust <eroderust@outlook.com>
2026-03-04 14:20:47 +0800

    chore: fix some minor issues in the comments
    Signed-off-by: eroderust <eroderust@outlook.com>

--- pkg/payerreport/workers/timers.go
+++ pkg/payerreport/workers/timers.go
@@ -12,7 +12,7 @@ var (
 	repeatIntervalMinutes uint32 = 60
 )
 
-// Distributes the minute of the hour that each node will run operations on.
+// Distributes the minute of the hour on which each node will run operations.
 // This is to avoid having many nodes do identical work at the same time, and potentially leading to
 // duplicate reports or submissions.
 //
@@ -27,14 +27,14 @@ func findNextRunTime(myNodeID uint32, workerID uint32) time.Time {
 
 	now := time.Now().UTC()
 
-	// Next run is at the „minuteOffset" of the current hour unless that
+	// Next run is at the "minuteOffset" of the current hour unless that
 	// time has already passed.
 	nextRun := time.Date(
 		now.Year(), now.Month(), now.Day(), now.Hour(), minuteOffset, 0, 0, time.UTC,
 	)
 
 	// Advance next run.
-	// The for loop cover corner cases where the next run is in the past.
+	// The for loop covers corner cases where the next run is in the past.
 	for !nextRun.After(now) {
 		nextRun = nextRun.Add(time.Duration(repeatIntervalMinutes) * time.Minute)
 	}

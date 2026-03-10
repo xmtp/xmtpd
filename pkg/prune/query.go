43ef0d6e905b905b6823af528c143efd85a63d74
Martin Kysel <code@martinkysel.com>
2026-03-10 17:57:09 -0400

    ctid trick

--- pkg/prune/query.go
+++ pkg/prune/query.go
@@ -12,19 +12,16 @@ func constructVariableMetaTableQuery(
 	maxSequenceId int64,
 ) string {
 	return fmt.Sprintf(`
-
-WITH to_delete AS (SELECT ge.originator_node_id,
-                          ge.originator_sequence_id
-                   FROM %s ge
-                   WHERE ge.expiry < EXTRACT(EPOCH FROM now())::bigint
-                     AND ge.originator_sequence_id <= %d
-                   ORDER BY ge.expiry, ge.originator_node_id, ge.originator_sequence_id
-                   LIMIT %d FOR UPDATE SKIP LOCKED)
-DELETE
-FROM %s ge
-    USING to_delete td
-WHERE ge.originator_node_id = td.originator_node_id
-  AND ge.originator_sequence_id = td.originator_sequence_id
-RETURNING ge.originator_node_id, ge.originator_sequence_id, ge.expiry;
+WITH to_delete AS (
+  SELECT ctid
+  FROM %s
+  WHERE expiry < EXTRACT(EPOCH FROM now())::bigint
+    AND originator_sequence_id <= %d
+  ORDER BY expiry
+  LIMIT %d
+  FOR UPDATE SKIP LOCKED
+)
+DELETE FROM %s
+WHERE ctid IN (SELECT ctid FROM to_delete);
 `, pq.QuoteIdentifier(tableName), maxSequenceId, batchSize, pq.QuoteIdentifier(tableName))
 }
